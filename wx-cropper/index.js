a09c41bdff7c9782daa2ef93f9bc302d95f03484
DavidDai <185098535@qq.com>
2018-09-12 12:02:41 +0800

    移除不必要代码，添加部分注释

--- wx-cropper/index.js
+++ wx-cropper/index.js
@@ -3,47 +3,48 @@
  */
 let SCREEN_WIDTH = 750
 let PAGE_X, // 手按下的x位置
-  PAGE_Y, // 手按下y的位置
-  PR = wx.getSystemInfoSync().pixelRatio, // dpi
-  T_PAGE_X, // 手移动的时候x的位置
-  T_PAGE_Y, // 手移动的时候Y的位置
-  CUT_L,  // 初始化拖拽元素的left值
-  CUT_T,  // 初始化拖拽元素的top值
-  CUT_R,  // 初始化拖拽元素的
-  CUT_B,  // 初始化拖拽元素的
-  CUT_W,  // 初始化拖拽元素的宽度
-  CUT_H,  //  初始化拖拽元素的高度
-  IMG_RATIO,  // 图片比例
-  IMG_REAL_W,  // 图片实际的宽度
-  IMG_REAL_H,   // 图片实际的高度
-  DRAFG_MOVE_RATIO = 750 / wx.getSystemInfoSync().windowWidth,  //移动时候的比例,
-  INIT_DRAG_POSITION = 200,   // 初始化屏幕宽度和裁剪区域的宽度之差，用于设置初始化裁剪的宽度
-  DRAW_IMAGE_W = 1080 // 设置生成的图片宽度
+    PAGE_Y, // 手按下y的位置
+    PR = wx.getSystemInfoSync().pixelRatio, // dpi
+    T_PAGE_X, // 手移动的时候x的位置
+    T_PAGE_Y, // 手移动的时候Y的位置
+    CUT_L,  // 初始化拖拽元素的left值
+    CUT_T,  // 初始化拖拽元素的top值
+    CUT_R,  // 初始化拖拽元素的
+    CUT_B,  // 初始化拖拽元素的
+    CUT_W,  // 初始化拖拽元素的宽度
+    CUT_H,  //  初始化拖拽元素的高度
+    IMG_RATIO,  // 图片比例
+    IMG_REAL_W,  // 图片实际的宽度
+    IMG_REAL_H,   // 图片实际的高度
+    DRAFG_MOVE_RATIO = 750 / wx.getSystemInfoSync().windowWidth,  //移动时候的比例,
+    INIT_DRAG_POSITION = 200,   // 初始化屏幕宽度和裁剪区域的宽度之差，用于设置初始化裁剪的宽度
+    DRAW_IMAGE_W = 1080 // 设置生成的图片宽度
 
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    // 之后可以动态替换
     imageSrc: 'http://www.bing.com/az/hprichbg/rb/BulgariaPerseids_ZH-CN11638911564_1920x1080.jpg',
+
+    // 是否显示图片(在图片加载完成之后设置为true)
     isShowImg: false,
+
     // 初始化的宽高
     cropperInitW: SCREEN_WIDTH,
     cropperInitH: SCREEN_WIDTH,
+
     // 动态的宽高
     cropperW: SCREEN_WIDTH,
     cropperH: SCREEN_WIDTH,
+
     // 动态的left top值
     cropperL: 0,
     cropperT: 0,
 
-    transL: 0,
-    transT: 0,
-
     // 图片缩放值
     scaleP: 0,
-    imageW: 0,
-    imageH: 0,
 
     // 裁剪框 宽高
     cutL: 0,
@@ -68,6 +69,10 @@ Page({
     this.loadImage();
 
   },
+
+  /**
+   * 选择本地图片
+   */
   getImage: function () {
     var _this = this
     wx.chooseImage({
@@ -79,6 +84,11 @@ Page({
       },
     })
   },
+
+  /**
+   * 初始化图片信息
+   * 获取图片内容，并初始化裁剪框
+   */
   loadImage: function () {
     var _this = this
     wx.showLoading({
@@ -135,24 +145,40 @@ Page({
       }
     })
   },
-  // 拖动时候触发的touchStart事件
+  
+  /**
+   * 拖动时候触发的touchStart事件
+   */
   contentStartMove(e) {
     PAGE_X = e.touches[0].pageX
     PAGE_Y = e.touches[0].pageY
   },
 
-  // 拖动时候触发的touchMove事件
+  /**
+   * 拖动时候触发的touchMove事件
+   */
   contentMoveing(e) {
     var _this = this
     var dragLengthX = (PAGE_X - e.touches[0].pageX) * DRAFG_MOVE_RATIO
     var dragLengthY = (PAGE_Y - e.touches[0].pageY) * DRAFG_MOVE_RATIO
-    // 左移
+
+    /**
+     * 这里有一个小的问题
+     * 移动裁剪框 ios下 x方向没有移动的差距
+     * y方向手指移动的距离远大于实际裁剪框移动的距离
+     * 但是在有些机型上又是没有问题的，小米4测试没有上下移动产生的偏差，模拟器ok，但是iphone8p确实是有的，虽然模拟器也ok
+     * 小伙伴有兴趣可以找找原因
+     */
+
+    // 左移右移
     if (dragLengthX > 0) {
       if (this.data.cutL - dragLengthX < 0) dragLengthX = this.data.cutL
     } else {
       if (this.data.cutR + dragLengthX < 0) dragLengthX = -this.data.cutR
     }
+    
 
+    // 上移下移
     if (dragLengthY > 0) {
       if (this.data.cutT - dragLengthY < 0) dragLengthY = this.data.cutT
     } else {
@@ -165,6 +191,11 @@ Page({
       cutB: this.data.cutB + dragLengthY
     })
 
+    // console.log('cutL', this.data.cutL)
+    // console.log('cutT', this.data.cutT)
+    // console.log('cutR', this.data.cutR)
+    // console.log('cutB', this.data.cutB)
+
     PAGE_X = e.touches[0].pageX
     PAGE_Y = e.touches[0].pageY
   },
@@ -173,7 +204,9 @@ Page({
 
   },
 
-  // 获取图片
+  /**
+   * 获取图片
+   */
   getImageInfo() {
     var _this = this
     wx.showLoading({
@@ -183,11 +216,12 @@ Page({
     const ctx = wx.createCanvasContext('myCanvas')
     ctx.drawImage(_this.data.imageSrc, 0, 0, IMG_REAL_W, IMG_REAL_H);
     ctx.draw(true, () => {
-      // 获取画布要裁剪的位置和宽度   均为百分比 * 画布中图片的宽度    保证了在微信小程序中裁剪的图片模糊  位置不对的问题 canvasT = (_this.data.cutT / _this.data.cropperH) * (_this.data.imageH / pixelRatio)
+      // 获取画布要裁剪的位置和宽度   均为百分比 * 画布中图片的宽度    保证了在微信小程序中裁剪的图片模糊  位置不对的问题
       var canvasW = ((_this.data.cropperW - _this.data.cutL - _this.data.cutR) / _this.data.cropperW) * IMG_REAL_W
       var canvasH = ((_this.data.cropperH - _this.data.cutT - _this.data.cutB) / _this.data.cropperH) * IMG_REAL_H
       var canvasL = (_this.data.cutL / _this.data.cropperW) * IMG_REAL_W
       var canvasT = (_this.data.cutT / _this.data.cropperH) * IMG_REAL_H
+      // 生成图片
       wx.canvasToTempFilePath({
         x: canvasL,
         y: canvasT,
@@ -209,7 +243,10 @@ Page({
     })
   },
 
-  // 设置大小的时候触发的touchStart事件
+  /**
+   * 设置大小的时候触发的touchStart事件
+   * 存数据
+   */
   dragStart(e) {
     T_PAGE_X = e.touches[0].pageX
     T_PAGE_Y = e.touches[0].pageY
@@ -219,7 +256,12 @@ Page({
     CUT_T = this.data.cutT
   },
 
-  // 设置大小的时候触发的touchMove事件
+  /**
+   * 设置大小的时候触发的touchMove事件
+   * 根据dragType判断类型
+   * 4个方向的边线拖拽效果
+   * 右下角按钮的拖拽效果
+   */
   dragMove(e) {
     var _this = this
     var dragType = e.target.dataset.drag
